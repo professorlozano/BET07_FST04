dd86b95766acb7327cf39c0db2a6f91a42dc2de7
Professor Lozano <luizmlozano@gmail.com>
2022-09-24 10:46:22 -0300

    encontro remoto 1 da UC12/14

---
+++ UC11/ER1/script_chapter.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE Chapter;
+
+USE Chapter;
+
+CREATE TABLE Livros
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Titulo VARCHAR(150) NOT NULL,
+	QuantidadePaginas INT,
+	Disponivel BIT
+);
+
+INSERT INTO Livros (Titulo, QuantidadePaginas, Disponivel)
+VALUES ('Titulo A', 120, 1);
+
+INSERT INTO Livros (Titulo, QuantidadePaginas, Disponivel)
+VALUES ('Titulo B', 220, 0);
+
+SELECT * FROM Livros;
+
