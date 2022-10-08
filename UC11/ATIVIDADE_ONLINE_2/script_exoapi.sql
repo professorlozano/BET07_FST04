e9a73b690fd1739a8c19ca629c5fe9497c4b3816
Professor Lozano <luizmlozano@gmail.com>
2022-10-08 10:54:40 -0300

    atv online 2 - parte 3

--- UC11/ATIVIDADE_ONLINE_2/script_exoapi.sql
+++ UC11/ATIVIDADE_ONLINE_2/script_exoapi.sql
@@ -18,3 +18,17 @@ INSERT INTO Projetos (Titulo, Status, Data_De_Inicio, Area)
 VALUES ('Projeto B', 'Iniciado', '18/02/2022', 'RH');
 
 select * from Projetos;
+
+
+CREATE TABLE Usuarios(
+	Id INT PRIMARY KEY IDENTITY,
+	Email VARCHAR(255) NOT NULL UNIQUE,
+	Senha VARCHAR(120) NOT NULL,
+	Tipo CHAR(1) NOT NULL
+);
+
+INSERT INTO Usuarios VALUES ('teste@email.com.br','1234','0');
+
+SELECT * FROM Usuarios;
+
+SELECT * FROM Usuarios WHERE email = 'teste@email.com.br' AND senha='1234';
