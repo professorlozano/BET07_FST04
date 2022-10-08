73740811a31019c4015b0920c41ac942b0a86dd2
Professor Lozano <luizmlozano@gmail.com>
2022-10-08 10:00:04 -0300

    atividade er3

---
+++ UC11/ER3/Chapter/Chapter/Chapter/Program.cs
@@ -0,0 +1,49 @@
+using Chapter.Contexts;
+using Chapter.Interfaces;
+using Chapter.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+
+});
+
+builder.Services.AddScoped<ChapterContext, ChapterContext>();
+builder.Services.AddScoped<LivroRepository, LivroRepository>();//verificar como transient
+
+builder.Services.AddTransient<IUsuarioRepository, UsuarioRepository>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
