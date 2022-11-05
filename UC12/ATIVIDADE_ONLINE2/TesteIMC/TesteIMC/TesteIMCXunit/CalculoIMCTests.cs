e637a6604271b3bd5037c645ca5f57471a31e166
Professor Lozano <luizmlozano@gmail.com>
2022-11-05 09:02:08 -0300

    resultado dos testes calculadora imc

---
+++ UC12/ATIVIDADE_ONLINE2/TesteIMC/TesteIMC/TesteIMCXunit/CalculoIMCTests.cs
@@ -0,0 +1,32 @@
+﻿using TesteIMC;
+using Xunit;
+
+namespace TesteIMCXunit
+{
+    public class CalculoIMCTests
+    {
+        //método de teste
+        [Fact]  
+        public void CalcularIMC_RetornaResultado()
+        {
+            //arrange - organizar, preparar o teste
+            double peso = 110;
+            double altura = 1.79;
+
+            //act - agir - execução/chamada do método
+            var resultado = Operacoes.CalcularIMC(peso, altura);
+
+            //assert - comportamento esperado, comportamento obtido
+            Assert.Equal(34.33, resultado);
+        }
+
+        [Theory]
+        [InlineData(80, 1.79, 24.97)]
+        [InlineData(100, 1.79, 31.21)]
+        public void CalcularIMC_RetornaResultado_ParaUmaListaDeValores(double primeiroNumero, double segundoNumero, double resultadoEsperado)
+        {
+            var resultadoDoIMC = Operacoes.CalcularIMC(primeiroNumero, segundoNumero);
+            Assert.Equal(resultadoEsperado, resultadoDoIMC);
+        }
+    }
+}
---
+++ UC12/ATIVIDADE_ONLINE2/TesteIMC/TesteIMC/TesteIMCXunit/ClassificarIMCTests.cs
@@ -0,0 +1,31 @@
+﻿using TestarIMC;
+using TesteIMC;
+using Xunit;
+
+namespace TesteIMCXunit
+{
+    public class ClassificarIMCTests
+    {
+        //método de teste
+        [Fact]
+        //descricao do teste
+        public void ClassificarIMC_RetornaResultado()
+        {
+            //arrange
+            double imc = 28;
+            //act
+            var classificacaoIMC = Operacoes.ClassificarIMC(imc);
+            //assert
+            Assert.Equal("Sobrepeso", classificacaoIMC);
+        }
+
+        [Theory]
+        [InlineData(28,"Sobrepeso")]
+        [InlineData(31,"Obesidade Grau I")]
+        public void ClassificarIMC_RetornaResultado_ParaUmaListaDeValores(double primeiroNumero, string resultadoEsperado)
+        {
+            var resultadoDoIMC = Operacoes.ClassificarIMC(primeiroNumero);
+            Assert.Equal(resultadoEsperado, resultadoDoIMC);
+        }
+    }
+}
