f03651194d1e0e2b3c1fc35159a731cb2e012d85
Professor Lozano <luizmlozano@gmail.com>
2022-11-05 09:38:45 -0300

    teste integracao

---
+++ UC12/ER3/Chapter/Chapter/Controllers/UsuarioController.cs
@@ -0,0 +1,101 @@
+﻿using Chapter.Interfaces;
+using Chapter.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepository _iusuarioRepository;
+
+        public UsuarioController(IUsuarioRepository iusuarioRepository)
+        {
+            _iusuarioRepository = iusuarioRepository;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_iusuarioRepository.Listar());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Usuario usuarioEncontrado = _iusuarioRepository.BuscarPorId(id);
+
+                if(usuarioEncontrado == null)
+                        return NotFound();
+                return Ok(usuarioEncontrado);                
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario usuario)
+        {
+            try
+            {
+                _iusuarioRepository.Cadastrar(usuario);
+                return StatusCode(201);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Alterar(int id, Usuario usuario)
+        {
+            try
+            {
+                _iusuarioRepository.Atualizar(id, usuario);
+                return Ok("Usuario Alterado");
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _iusuarioRepository.Deletar(id);
+                return Ok("Usuario Deletado");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ UC12/ER3/Chapter/TestProject1/Controller/LoginControllerTeste.cs
@@ -0,0 +1,70 @@
+﻿using Chapter.Controllers;
+using Chapter.Interfaces;
+using Chapter.Models;
+using Chapter.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace TestProject1.Controller
+{
+    public class LoginControllerTeste
+    {
+        [Fact]
+        public void LoginController_Retornar_Usuario_Invalido()
+        {
+            //Arrange
+            var fakeRepository = new Mock<IUsuarioRepository>();
+            fakeRepository.Setup(x => x.Login(It.IsAny<string>(), It.IsAny<string>())).Returns((Usuario)null);
+
+            LoginVIewModel dadosLogin = new LoginVIewModel();
+
+            dadosLogin.Email = "email@email.com";
+            dadosLogin.Senha = "123";
+
+            var controller = new LoginController(fakeRepository.Object);
+
+            //Act
+            var resultado = controller.Login(dadosLogin);
+
+            //Assert
+            Assert.IsType<UnauthorizedObjectResult>(resultado);
+        }
+
+
+        [Fact]
+        public void LoginController_Retornar_Token()
+        {
+            //Arrange
+            Usuario usuarioRetorno = new Usuario();
+            usuarioRetorno.Email = "email@email.com";
+            usuarioRetorno.Senha = "1234";
+            usuarioRetorno.Tipo = "0";
+
+            var fakeRepository = new Mock<IUsuarioRepository>();
+            fakeRepository.Setup(x => x.Login(It.IsAny<string>(), It.IsAny<string>())).Returns(usuarioRetorno);
+
+            string issuerValidacao = "chapter.webapi";
+
+            LoginVIewModel dadosLogin = new LoginVIewModel();
+
+            dadosLogin.Email = "batata";
+            dadosLogin.Senha = "123";
+
+            var controller = new LoginController(fakeRepository.Object);
+
+            //Act
+            OkObjectResult resultado = (OkObjectResult)controller.Login(dadosLogin);
+
+            string token = resultado.Value.ToString().Split(' ')[3];
+
+            var jwtHandler = new JwtSecurityTokenHandler();
+            var tokenJwt = jwtHandler.ReadJwtToken(token);
+
+            //Assert
+            Assert.Equal(issuerValidacao, tokenJwt.Issuer);
+        }
+
+
+    }
+}
